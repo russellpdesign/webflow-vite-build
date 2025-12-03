416a600af40ed48cb30524fae66a34315bd9a9e1
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-12-02 21:14:24 -0800

    updated pages/donovan-mitchell.js to include new photoTravelOver section

--- src/pages/donovan-mitchell.js
+++ src/pages/donovan-mitchell.js
@@ -1,15 +1,14 @@
 import ScrollEngine from "../engine/ScrollEngine.js";
 import SmoothScroll from "../engine/SmoothScroll.js";
 import HomeScrollSection from "../sections/HomeScrollSection.js";
+import PhotoTravelOver from "../sections/PhotoTravelOver.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   const smooth = new SmoothScroll({ ease: 0.08 });
   const engine = new ScrollEngine({ smooth });
 
-  const section = new HomeScrollSection({
-    el: ".home-scroll-section.is-don"
-  });
+  engine.register(new HomeScrollSection({ el: ".home-scroll-section.is-don" }));
+  engine.register(new PhotoTravelOver({ el: ".sticky-section.heroic-members-wrapper.reversed" }));
 
-  engine.register(section);
   engine.start();
 });
