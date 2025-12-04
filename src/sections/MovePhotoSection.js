af66e6c120168198fb5cf7ce9badd8134dbe287c
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-12-04 14:34:41 -0800

    removing buffer

--- src/sections/MovePhotoSection.js
+++ src/sections/MovePhotoSection.js
@@ -34,9 +34,9 @@ export default class MovePhotoSection extends StickyBaseSection {
     this.imageRevealSection       = document.querySelector(".double-wide-reveal-img");
     this.leftSideImageHide        = document.querySelector("#left-side-hide");
 
-    // This section moves FAST during reverse scroll; we give it a larger buffer.
-    this.baseReleaseBuffer = 30;        // default from base is 20
-    this.releaseMultiplier = 350;       // default from base is 300
+    // // This section moves FAST during reverse scroll; we give it a larger buffer.
+    // this.baseReleaseBuffer = 30;        // default from base is 20
+    // this.releaseMultiplier = 350;       // default from base is 300
 
     this.enabled = true;
   }
@@ -116,7 +116,7 @@ export default class MovePhotoSection extends StickyBaseSection {
       case "MAIN_MOVE": {
         const localT = clamp01(t / this.timeline[0].end);
 
-        const x       = mapRange(localT, 0, 1, 0, 100);
+        const x = mapRange(localT, 0, 1, 0, 100);
         const behindX = 100 - x;
         const opacity = mapRange(localT, 0, 1, 100, 0);
 
