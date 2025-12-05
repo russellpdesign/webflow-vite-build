9988d6d7c20396775cf6c8b185605f2bfe32dafe
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-12-04 16:49:32 -0800

    updating offset

--- src/sections/MovePhotoSection.js
+++ src/sections/MovePhotoSection.js
@@ -48,7 +48,7 @@ export default class MovePhotoSection extends StickyBaseSection {
     super.measure();
 
     // custom timing offset into the pin start (old 138vh logic simplified)
-    this.pinOffset = 0;
+    this.pinOffset = -window.innerHeight * 4;
 
     // shift pin start to match your original flow
     this.start += this.pinOffset;
