cb50d023f3147f987b3cf67b784fb674f581bc40
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-01-07 17:10:35 -0800

    added log for triggers array

--- src/sections/PhotoOverlapSection.js
+++ src/sections/PhotoOverlapSection.js
@@ -39,6 +39,7 @@ export default class PhotoOverlapSection extends BaseSection {
     if(!this.enabled) return;
 
     console.log(`this is the start of the photoOverlap section: ${this.start}`);
+    console.log(triggers);
     // console.table(`${this, el, this.el}`);
 
       // compute progress for image translation
