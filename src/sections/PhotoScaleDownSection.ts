af42062e6a225db2ab6058cbd4e4bd9be9678df1
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-01-31 13:48:22 -0800

    testing

--- src/sections/PhotoScaleDownSection.ts
+++ src/sections/PhotoScaleDownSection.ts
@@ -104,7 +104,7 @@ export default class PhotoScaleDown extends BaseSection {
     this.scaleDownImgHeightStartingValue = 120; // in percentage hardcoded but can retreive like window.getComputedStyles(this.scaleDownImg); along with the parents styles and then converting to percentage. We know from webflow its 120%
     this.scaleDownImgHeightEndingValue = 150; // in percentage
 
-    console.log(this.opacityToggleEndpoint, window.scrollY)
+    console.log(`this.end: ${this.end}, this.opacityToggleEndpoint: ${this.opacityToggleEndpoint}, scrollY:`, window.scrollY)
   }
 
   update(scrollY: number): void {
