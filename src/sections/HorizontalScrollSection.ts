4d2f11783e174a946ed58106d6b7ef142481838b
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-04-21 12:22:41 -0700

    update

--- src/sections/HorizontalScrollSection.ts
+++ src/sections/HorizontalScrollSection.ts
@@ -138,7 +138,7 @@ update(scrollY: number): void {
     }
 
     private _deactivate(i: number): void {
-      this.bigTexts[i-1].classList.remove("active");
+      // this.bigTexts[i+1].classList.remove("active");
       this.mediumBigTexts[i].classList.remove("active");
       this.productDescs[i].classList.remove("active");
     }
