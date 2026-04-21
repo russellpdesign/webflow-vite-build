21439e6f1e2d99b22da4d63acfd7c270668fcb12
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-04-21 13:36:15 -0700

    update

--- src/sections/HorizontalScrollSection.ts
+++ src/sections/HorizontalScrollSection.ts
@@ -108,15 +108,15 @@ update(scrollY: number): void {
         if(newActiveIndex < this.activeSectionIndex) {
           console.log("i should do nothing!")
           this._activate(newActiveIndex);
+          this.activeSectionIndex = newActiveIndex;
         }
 
         if(newActiveIndex > this.activeSectionIndex) {
           console.log("i should animate our eyebrow, desc and dropdown in, and remove our big title")
           this._activate(newActiveIndex);
+          this.activeSectionIndex = newActiveIndex;
         }
       }
-      
-      this.activeSectionIndex = newActiveIndex;
     }
 
 
