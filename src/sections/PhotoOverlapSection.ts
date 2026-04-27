0e92573dc0a3422317273c876867c9567876cdf0
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-04-27 16:04:17 -0700

    bug fix

--- src/sections/PhotoOverlapSection.ts
+++ src/sections/PhotoOverlapSection.ts
@@ -56,8 +56,7 @@ export default class PhotoOverlapSection extends BaseSection {
 
     // All images that participate in the overlap animation
     this.initialImages = Array.from(this.sectionTrigger.querySelectorAll<HTMLElement>(".sticky-img-container"));
-    this.typeofInitialImages = this.initialImages.typeof;
-    console.log(`inside of class instance measure this.initialImages: ${this.typeofInitialImages}`);
+    console.log(`inside of class instance in class constructor this.initialImages: ${typeof(initialImages)}`);
     this.leftSideImages = this.initialImages.slice(0, -1);
 
     // console.log(this.initialImages);
